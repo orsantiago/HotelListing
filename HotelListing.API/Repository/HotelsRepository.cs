5a79db1d0adb271d3bdca53faa9a142ba34984b2
Orlando Santiago <30550798+Rolandjacks@users.noreply.github.com>
2022-05-17 09:39:19 -0400

    section 7 - completed

---
+++ HotelListing.API/Repository/HotelsRepository.cs
@@ -0,0 +1,20 @@
+﻿using HotelListing.API.Contracts;
+using HotelListing.API.Data;
+
+namespace HotelListing.API.Repository
+{
+    public class HotelsRepository : GenericRepository<Hotel>, IHotelsRepository
+    {
+        private readonly HotelListingDbContext _context;
+
+        public HotelsRepository(HotelListingDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public Task<Hotel> GetDetails(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
